c4a032667d3021fcfb3bc9a1c643fbe69871f300
almad <none@none>
2006-12-29 01:29:10 +0100

    [svn] Tagging 0.2 alpha 2
    --HG--
    branch : sneakylang
    rename : sneakylang/trunk/sneakylang/test/test_macro.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/test_macro.py
    rename : sneakylang/trunk/setup.py => sneakylang/tags/sneakylang-0.2_alpha2/setup.py
    rename : sneakylang/trunk/sneakylang/err.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/err.py
    rename : sneakylang/trunk/sneakylang/register.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/register.py
    rename : sneakylang/trunk/sneakylang/test/module_test.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/module_test.py
    rename : sneakylang/trunk/sneakylang/test/test_register.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/test_register.py
    rename : sneakylang/trunk/sneakylang/test/test_macro_syntax.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/test_macro_syntax.py
    rename : sneakylang/trunk/sneakylang/__init__.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/__init__.py
    rename : sneakylang/trunk/license.txt => sneakylang/tags/sneakylang-0.2_alpha2/license.txt
    rename : sneakylang/trunk/sneakylang/node.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/node.py
    rename : sneakylang/trunk/sneakylang/expanders.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/expanders.py
    rename : sneakylang/trunk/sneakylang/macro.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/macro.py
    rename : sneakylang/trunk/sneakylang/test/test_simple_wiki.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/test_simple_wiki.py
    rename : sneakylang/trunk/sneakylang/macro_caller.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/macro_caller.py
    rename : sneakylang/trunk/sneakylang/parser.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/parser.py
    rename : sneakylang/trunk/sneakylang/test/test_parser.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/test_parser.py
    rename : sneakylang/trunk/sneakylang/document.py => sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/document.py

---
+++ sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/parser.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+""" Parser transforming input stream to DOM
+"""
+
+###
+# SneakyLang: Extensible WikiFramework
+#Copyright (C) 2006 Lukas "Almad" Linhart http://www.almad.net/
+# and contributors, for complete list see
+# http://projects.almad.net/c~/projects/sneakylang/sneakylang/testzechtile/wiki/Contributors
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+###
+
+import logging
+
+from err import ParserRollback
+
+import macro
+from node import TextNode
+from register import Register
+from macro_caller import expand_macro_from_stream
+
+class Parser(object):
+    """ All parsers should derivate from this class """
+    start = []
+    macro = None
+
+    def __init__(self, stream, parent_parser, chunk, register):
+        """ Parse is taking activity in DOM dom because of chunk resolved """
+        self.chunk = chunk
+        self.parent_parser = parent_parser
+        self.argument_string = ''
+        self.init()
+        self.stream = stream[len(chunk):]
+        self._register = register
+
+    def init(self):
+        """ Something to do after init? ,) """
+        pass
+
+    def begin_parse(self):
+        """ Begin parsing, set up needed things, determine whether to append
+        stream to chunk or mark chunk as node_start
+        """
+        pass
+
+    def _get_macro(self):
+        return self.macro.argument_call(self.argument_string, self.register)
+
+    def resolve_argument_string(self):
+        """ Resolve transform content to argument string which would be used if calling macro by macro syntax """
+
+    def get_macro(self):
+        """ Return properly istantiazed macro and new stream """
+        self.begin_parse()
+        self.resolve_argument_string()
+        return (self._get_macro(), self.stream)
+
+    def parse(self):
+        macro = self.get_macro()
+        return macro.expand()
+#        self.dom_tree = self.call_macro()
+#        return self.dom_tree
+
+    def get_register(self):
+        if self._register is not None:
+            return self._register
+        else:
+            return self.register_map[self.__class__]
+
+    register = property(fget=get_register)
+
+
+def _get_text_node(stream, register, register_map, force_first_char=False, opened_text_node=None):
+    if opened_text_node is None:
+        tn = TextNode()
+    else:
+        tn = opened_text_node
+
+    if force_first_char is True:
+        tn.content = ''.join([tn.content, stream[0:1]])
+        stream = stream[1:]
+
+    while True:
+        try:
+            res = register.resolve_macro(stream)
+        except ParserRollback:
+            pass
+        else:
+            if res != (None, None):
+                break
+        if len(stream) == 0:
+            break
+        tn.content = ''.join([tn.content, stream[0:1]])
+        stream = stream[1:]
+    return (tn, stream)
+
+def parse(stream, register_map, register=None, parsers=None):
+    if register is None:
+        register = Register([p for p in register_map])
+        register.visit_register_map(register_map)
+        if parsers is not None:
+            register.add_parsers(parsers)
+    opened_text_node = None
+    nodes = []
+    while len(stream) > 0:
+        macro, stream_new = register.resolve_macro(stream)
+        if macro is not None and stream_new is not None:
+            logging.debug('Resolved macro %s' % macro)
+            try:
+                res = macro.expand()
+                logging.debug('Appending %s' % res)
+                nodes.append(res)
+                stream = stream_new
+                opened_text_node = None
+            except ParserRollback:
+                logging.debug('Catched ParseRollback, forcing text char')
+                node, stream = _get_text_node(stream, register, register_map, True, opened_text_node=opened_text_node)
+                if opened_text_node is None:
+                    nodes.append(node)
+                opened_text_node=node
+        else:
+            logging.debug('Parser is None (not resolved), adding TextNode.')
+            node, stream = _get_text_node(stream, register, register_map, opened_text_node=opened_text_node)
+            if opened_text_node is None:
+                nodes.append(node)
+            opened_text_node=node
+    return nodes
---
+++ sneakylang/tags/sneakylang-0.2_alpha2/sneakylang/test/test_macro_syntax.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Test macro syntax, now being build-in into main parser """
+
+###
+# SneakyLang: Extensible WikiFramework
+#Copyright (C) 2006 Lukas "Almad" Linhart http://www.almad.net/
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+###
+
+from os import pardir, tmpfile, remove
+from os.path import join
+import sys
+sys.path.insert(0, join(pardir, pardir))
+import logging
+import re
+
+from unittest import main,TestCase
+from module_test import *
+
+#logging.basicConfig(level=logging.DEBUG)
+
+from sneakylang import parse, RegisterMap, Document
+
+class TestSimpleResolving(TestCase):
+    def setUp(self):
+        self.register_map = RegisterMap({
+            ParagraphMacro : Register([StrongMacro]),
+            StrongMacro : Register([]),
+            Document : Register([ParagraphMacro])
+        })
+
+        self.expanderMap = {
+             'docbook5' : {
+                 ParagraphNode : ParagraphDocbookExpand,
+                 TextNode : TextNodeExpander             }
+        }
+
+    def testProperResolving(self):
+        s = '((odstavec text odstavce))'
+        o = parse(s, self.register_map)
+        self.assertEquals(len(o), 1)
+        self.assertEquals(o[0].__class__, ParagraphNode)
+        self.assertEquals(o[0].children[0].__class__, TextNode)
+        self.assertEquals(o[0].children[0].content, 'text odstavce')
+        # nefunguje, nutne udelat parsovani vnorenych maker
+#        s = '((odstavec ((silne silny)) text odstavce))'
+#        o = parse(s, self.register_map)
+#        self.assertEquals(len(o), 1)
+#        self.assertEquals(o[0].__class__, ParagraphNode)
+#        self.assertEquals(o[0].children[0].__class__, Strong)
+#        self.assertEquals(o[0].children[0].children[0].__class__, TextNode)
+#        self.assertEquals(o[0].children[0].children[0].content, 'silny')
+#        self.assertEquals(o[0].children[1].__class__, TextNode)
+#        self.assertEquals(o[0].children[1].content, ' text odstavce')
+
+
+if __name__ == "__main__":
+    main()
