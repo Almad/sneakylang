e0e3d02b152cc6a99c1b790686ff6dc2be24b098
almad <none@none>
2006-12-29 01:30:43 +0100

    [svn] Properly tagging 0.2 alpha 1
    --HG--
    branch : sneakylang
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/macro.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/macro.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/expanders.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/expanders.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/test/module_test.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/test/module_test.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/setup.py => sneakylang/tags/sneakylang-0.2_alpha1/setup.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/macro_caller.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/macro_caller.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/test/test_macro_syntax.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/test/test_macro_syntax.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/node.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/node.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/classregistry.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/classregistry.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/test/test_macro.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/test/test_macro.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/parser.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/parser.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/test/test_simple_wiki.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/test/test_simple_wiki.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/__init__.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/__init__.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/test/test_parser.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/test/test_parser.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/err.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/err.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/document.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/document.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/test/test_register.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/test/test_register.py
    rename : sneakylang/tags/sneakylang-0.1_alpha1/license.txt => sneakylang/tags/sneakylang-0.2_alpha1/license.txt
    rename : sneakylang/tags/sneakylang-0.1_alpha1/sneakylang/register.py => sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/register.py

---
+++ sneakylang/tags/sneakylang-0.2_alpha1/sneakylang/macro.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+""" Macro superclass and default Document and Macro classes """
+
+###
+# SneakyLang: Extensible WikiFramework
+#Copyright (C) 2006 Lukas "Almad" Linhart http://www.almad.net/
+# and contributors, for complete list see
+# http://projects.almad.net/sneakylang/wiki/Contributors
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+###
+
+import re
+import logging
+
+from err import *
+import node
+from macro_caller import ARGUMENT_SEPARATOR
+
+class Macro(object):
+    """ All macros should derive from this class """
+    name = None # define macro name
+    help = """<this macro haven't specified usage example>"""
+    parsers_allowed = None
+
+    def __init__(self, parser, register_map):
+        self.parser = parser
+        self.register_map = register_map
+
+    @classmethod
+    def parse_argument_string(self, argument_string):
+        """ Return list of arguments. Uses ARGUMENT_SEPARATOR as argument separator.
+        #TODO:
+        By default, closing text in double quotes (") causes treating it as single argument, even if it contains
+        argument separator. Nested quotes must not be escaped unless containting quote followed by argument
+        separator; escape char is backslash (\)
+        """
+        return argument_string.split(ARGUMENT_SEPARATOR)
+
+    @classmethod
+    def argument_call(cls, argument_string, register=None, macro_instance=None):
+        """ This function do proper call to expand with properly parsed argument_string.
+        If you want to modify how argument string is parsed, overwrite parse_argument_string classmethod.
+        If macro_instance is not given, macro is instantiazed with parser and register_map extracted
+        from register argument.
+        With or without instance, result of self.expand is returned."""
+        if macro_instance is None:
+            if register is None:
+                raise ValueError, 'Either macro_instance or register must be given for performing argument_call'
+            macro_instance = cls(register.parser_name_map[cls.name], register.register_map)
+        if argument_string is None:
+            return macro_instance.expand()
+        else:
+            return macro_instance.expand(*cls.parse_argument_string(argument_string))
+
+    def expand(self, *args, **kwargs):
+        """ Macro with arguments resolved; macro should expand themselves to Nodes and append to DOM """
+        raise NotImplementedError
+
+    def _get_register(self):
+        """ Property function, use .register attribute instead """
+        return self.register_map[self.parser]
+
+    register = property(fget=_get_register)
