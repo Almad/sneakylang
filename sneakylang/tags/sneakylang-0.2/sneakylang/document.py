73319cc349be921563aa4814dd8532040bd1bdf9
almad <none@none>
2007-04-12 17:08:08 +0200

    [svn] Tagging 0.2 release
    --HG--
    branch : sneakylang
    rename : sneakylang/trunk/sneakylang/macro.py => sneakylang/tags/sneakylang-0.2/sneakylang/macro.py
    rename : sneakylang/trunk/sneakylang/err.py => sneakylang/tags/sneakylang-0.2/sneakylang/err.py
    rename : sneakylang/trunk/sneakylang/test/test_macro_syntax.py => sneakylang/tags/sneakylang-0.2/sneakylang/test/test_macro_syntax.py
    rename : sneakylang/trunk/sneakylang/parser.py => sneakylang/tags/sneakylang-0.2/sneakylang/parser.py
    rename : sneakylang/trunk/sneakylang/test/test_simple_wiki.py => sneakylang/tags/sneakylang-0.2/sneakylang/test/test_simple_wiki.py
    rename : sneakylang/trunk/sneakylang/register.py => sneakylang/tags/sneakylang-0.2/sneakylang/register.py
    rename : sneakylang/trunk/sneakylang/document.py => sneakylang/tags/sneakylang-0.2/sneakylang/document.py
    rename : sneakylang/trunk/sneakylang/test/test_parser.py => sneakylang/tags/sneakylang-0.2/sneakylang/test/test_parser.py
    rename : sneakylang/trunk/sneakylang/macro_caller.py => sneakylang/tags/sneakylang-0.2/sneakylang/macro_caller.py
    rename : sneakylang/trunk/license.txt => sneakylang/tags/sneakylang-0.2/license.txt
    rename : sneakylang/trunk/sneakylang/test/module_test.py => sneakylang/tags/sneakylang-0.2/sneakylang/test/module_test.py
    rename : sneakylang/trunk/sneakylang/__init__.py => sneakylang/tags/sneakylang-0.2/sneakylang/__init__.py
    rename : sneakylang/trunk/sneakylang/test/test_register.py => sneakylang/tags/sneakylang-0.2/sneakylang/test/test_register.py
    rename : sneakylang/trunk/sneakylang/node.py => sneakylang/tags/sneakylang-0.2/sneakylang/node.py
    rename : sneakylang/trunk/sneakylang/test/test_macro.py => sneakylang/tags/sneakylang-0.2/sneakylang/test/test_macro.py
    rename : sneakylang/trunk/setup.py => sneakylang/tags/sneakylang-0.2/setup.py
    rename : sneakylang/trunk/sneakylang/expanders.py => sneakylang/tags/sneakylang-0.2/sneakylang/expanders.py

---
+++ sneakylang/tags/sneakylang-0.2/sneakylang/document.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+""" Default document macro/nodes """
+
+###
+# SneakyLang: Extensible WikiFramework
+#Copyright (C) 2006 Lukas "Almad" Linhart http://www.almad.net/
+# and contributors, for complete list see
+# http://projects.almad.net/sneakylang/wiki/Contributors
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+###
+
+import logging
+
+from macro import Macro
+from parser import Parser, parse
+from node import Node
+
+class Document(Macro):
+    name = 'document'
+    help = '<toto makro se nikdy nepouziva explicitne>'
+
+    def parse_argument_string(self, argument_string):
+        self.arguments = [argument_string]
+
+    def expand_to_nodes(self, content):
+        doc = DocumentNode()
+        logging.debug('Creating document node and parsing document')
+        res = parse(content, self.register_map, self.register)
+        for node in res:
+            if node is not None:
+                doc.add_child(node)
+        return doc
+
+
+class DocumentParser(Parser):
+    start = None
+    macro = Document
+
+    def resolve_argument_string(self):
+        self.argument_string = self.stream
+        self.stream = ''
+
+#    def call_macro(self):
+#        """ Do proper call to related macro(s) """
+#        return self.macro(self.register, self.register_map).expand(self.content, self.parser)
+
+class DocumentNode(Node): pass
---
+++ sneakylang/tags/sneakylang-0.2/sneakylang/test/test_parser.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Test Parsers
+"""
+
+###
+# SneakyLang: Extensible WikiFramework
+#Copyright (C) 2006 Lukas "Almad" Linhart http://www.almad.net/
+#
+#This library is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License as published by the Free Software Foundation; either
+#version 2.1 of the License, or (at your option) any later version.
+#
+#This library is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#Lesser General Public License for more details.
+#
+#You should have received a copy of the GNU Lesser General Public
+#License along with this library; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+###
+
+from os import pardir, tmpfile, remove
+from os.path import join
+import sys
+sys.path.insert(0, join(pardir, pardir))
+import logging
+import re
+
+from unittest import main,TestCase
+
+from module_test import *
+
+from sneakylang.macro import Macro
+from sneakylang.node import Node
+from sneakylang.parser import *
+from sneakylang.register import Register, RegisterMap
+
+#logging.basicConfig(level=logging.DEBUG)
+
+class TestParser(TestCase):
+
+    def setUp(self):
+        self.p = DummyParser('', None, '####', Register([DummyMacro]))
+
+    def testMacroResolving(self):
+        self.assertEquals(self.p.get_macro()[0].__class__, DummyMacro)
+
+class TestParserCapabilities(TestCase):
+
+    def testParserTransform(self):
+        register_map = RegisterMap({DummyMacro: Register([])})
+        res = parse('####',register_map, parsers=[DummyParser])
+        self.assertEquals(len(res), 1)
+        self.assertEquals(res[0].__class__, DummyNode)
+
+    def testUnbreakedTextNodeProcessing(self):
+        txt = '= jakoby nadpis\n= jakoby druhy nadpis'
+        res = parse(txt, RegisterMap({NadpisMacro:Register()}))
+
+        self.assertEquals(1, len(res))
+        self.assertEquals(txt, res[0].content)
+
+if __name__ == "__main__":
+    main()
